f015a9088aa7bb119b429ed0bd8bcd3bdfb02471
Calvin <calvin.peak@gmail.com>
2016-06-22 10:47:40 +0900

    Category control with config file.

---
+++ FakeKKTown/Category.swift
@@ -0,0 +1,25 @@
+//
+//  Category.swift
+//  CarouselPratice
+//
+//  Created by Calvin on 6/15/16.
+//  Copyright © 2016 CapsLock. All rights reserved.
+//
+
+import Foundation
+
+import ObjectMapper
+
+struct Category: Mappable {
+    var title: String!
+    var icon: String!
+    
+    init?(_ map: Map) {
+        
+    }
+    
+    mutating func mapping(map: Map) {
+        title <- map["title"]
+        icon <- map["icon"]
+    }
+}
